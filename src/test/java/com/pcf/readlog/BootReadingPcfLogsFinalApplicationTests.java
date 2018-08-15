5b4199dc129a8d66fbf159e2957e1c24e4c055c7
GanesanNS <Ganesan.NatarajanShanmugam@team.telstra.com>
2018-08-15 17:35:16 +0530

    Add workflow : Change to JUnit Test

--- src/test/java/com/pcf/readlog/BootReadingPcfLogsFinalApplicationTests.java
+++ src/test/java/com/pcf/readlog/BootReadingPcfLogsFinalApplicationTests.java
@@ -1,19 +1,14 @@
 package com.pcf.readlog;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertTrue;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
 public class BootReadingPcfLogsFinalApplicationTests {
 
-	@Test
-	public void contextLoads() {
+    @Test
+    public void contextLoads() {
         assertTrue(true);
-	}
+    }
 
 }
